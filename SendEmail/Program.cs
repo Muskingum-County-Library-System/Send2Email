3ec05584ad08d1ad1f8aca2641420199f129da80
unknown <dylanbanta@gmail.com>
2024-05-24 22:29:09 -0400

    Updated to use Encryption for smtp_password, and added some QOL features

--- SendEmail/Program.cs
+++ SendEmail/Program.cs
@@ -5,21 +5,22 @@ using System.Windows.Forms;
 using System.Globalization;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace SendEmail
 {
-     class Program
+    class Program
     {
         #region Global Variables
-        //File Explorer Path
-        //C:\Users\[USER]\AppData\Local\Send2Email
+        // File Explorer Path
+        // C:\Users\[USER]\AppData\Local\Send2Email
         public static string appPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Send2Email\\";
 
-        //My Pictures Path
-        //C:\Users\[USER]\My Pictures
+        // My Pictures Path
+        // C:\Users\[USER]\My Pictures
         public static string picPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
 
-        //Variables to be loaded from config file
+        // Variables to be loaded from config file
         public static string configName = "cfg";
         public static string configFileExt = "json";
         public static string configFileName = configName + "." + configFileExt;
@@ -33,28 +34,113 @@ namespace SendEmail
         public static string mail_body;
         public static string mail_delivery;
         public static string file_extensions;
+
+        // Encryption keys (8 bytes for DES)
+        public static string key_secret = "8byteKey";
+        public static string key_public = "byte8Key";
         #endregion
 
         #region Main Method
         [STAThread]
-        static void Main() //Main method, this runs as soon as the software is started
+        static void Main()
         {
-            //Set up the basic requirements for a WinForms application
+            // Set up the basic requirements for a WinForms application
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            
-            bool cont = Form3.LoadConfig(); //Load Configuration method
-            if (cont) //If Config exists
+            bool cont = Form3.LoadConfig(); // Load Configuration method
+            if (cont) // If Config exists
             {
-                Application.Run(new Form1()); //Open Form1 (Main Window)
+                if (string.IsNullOrEmpty(smtp_pass)) // Check if password is empty
+                {
+                    // Run first time setup
+                    Application.Run(new Form3());
+                }
+                else
+                {
+                    Application.Run(new Form1()); // Open Form1 (Main Window)
+                }
             }
-            else //If Config Doesn't Exist
+            else // If Config Doesn't Exist
             {
-                //Display an error message before closing the application
+                // Display an error message before closing the application
                 MessageBox.Show("A critical error has occurred. Configuration file is missing. Send2Email will not function without this configuration file. Please contact IT to resolve this issue.");
-                Application.Exit(); //Exit the application
+                Application.Exit(); // Exit the application
+            }
+        }
+        #endregion
+
+        #region Encryption Methods
+        public static string Encrypt(string input)
+        {
+            try
+            {
+                string textToEncrypt = input;
+                string ToReturn = "";
+                byte[] secretkeyByte = System.Text.Encoding.UTF8.GetBytes(key_secret);
+                byte[] publickeybyte = System.Text.Encoding.UTF8.GetBytes(key_public);
+                byte[] inputbyteArray = System.Text.Encoding.UTF8.GetBytes(textToEncrypt);
+
+                using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+                {
+                    des.Mode = CipherMode.CBC;
+                    des.Padding = PaddingMode.PKCS7;
+                    des.Key = publickeybyte;
+                    des.IV = secretkeyByte;
+
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(inputbyteArray, 0, inputbyteArray.Length);
+                            cs.FlushFinalBlock();
+                            ToReturn = Convert.ToBase64String(ms.ToArray());
+                        }
+                    }
+                }
+
+                return ToReturn;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+        }
+
+        public static string Decrypt(string input)
+        {
+            try
+            {
+                string textToDecrypt = input;
+                string ToReturn = "";
+                byte[] privatekeyByte = System.Text.Encoding.UTF8.GetBytes(key_secret);
+                byte[] publickeybyte = System.Text.Encoding.UTF8.GetBytes(key_public);
+                byte[] inputbyteArray = Convert.FromBase64String(textToDecrypt.Replace(" ", "+"));
+
+                using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+                {
+                    des.Mode = CipherMode.CBC;
+                    des.Padding = PaddingMode.PKCS7;
+                    des.Key = publickeybyte;
+                    des.IV = privatekeyByte;
+
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(inputbyteArray, 0, inputbyteArray.Length);
+                            cs.FlushFinalBlock();
+                            ToReturn = System.Text.Encoding.UTF8.GetString(ms.ToArray());
+                        }
+                    }
+                }
+
+                return ToReturn;
+            }
+            catch (Exception ae)
+            {
+                throw new Exception(ae.Message, ae.InnerException);
             }
         }
         #endregion
@@ -62,18 +148,16 @@ namespace SendEmail
         #region Email Methods
 
         #region IsValidEmail
-        //Checks if the email address entered is valid.
-        //Source https://docs.microsoft.com/en-us/dotnet/standard/base-types/how-to-verify-that-strings-are-in-valid-email-format
-        public static bool IsValidEmail(string email) //Accepts an email string and returns true if email is valid
+        // Checks if the email address entered is valid.
+        public static bool IsValidEmail(string email)
         {
-            if (string.IsNullOrWhiteSpace(email)) //Null strings aren't valid so return false
+            if (string.IsNullOrWhiteSpace(email)) // Null strings aren't valid so return false
                 return false;
 
             try
             {
                 // Normalize the domain
-                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
-                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper, RegexOptions.None, TimeSpan.FromMilliseconds(200));
 
                 // Examines the domain part of the email and normalizes it.
                 string DomainMapper(Match match)
@@ -98,9 +182,7 @@ namespace SendEmail
 
             try
             {
-                return Regex.IsMatch(email,
-                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
-                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+                return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
             }
             catch (RegexMatchTimeoutException)
             {
@@ -110,20 +192,19 @@ namespace SendEmail
         #endregion
 
         #region Attachments
-        //Checks files in Windows Pictures folder
-        //returns filepaths with matching extensions
+        // Checks files in Windows Pictures folder
+        // returns filepaths with matching extensions
         public static string[] Attachments()
         {
-
             string[] extensions;
-            List<string> output = new List<string>(); //Holds the output
+            List<string> output = new List<string>(); // Holds the output
 
             try
             {
                 extensions = file_extensions.Split(",");
                 for (int e = 0; e < extensions.Length; e++)
                 {
-                    extensions[e] = extensions[e].Trim(' '); //Remove any whitespace
+                    extensions[e] = extensions[e].Trim(' '); // Remove any whitespace
                 }
             }
             catch (Exception ex)
@@ -133,195 +214,64 @@ namespace SendEmail
                 extensions = fill;
             }
 
-            string[] files = Directory.GetFiles(picPath); //Get array of all files in Pictures folder
+            string[] files = Directory.GetFiles(picPath); // Get array of all files in Pictures folder
 
-            for (int i = 0; i < files.Length; i++) //Loop through all files
+            for (int i = 0; i < files.Length; i++) // Loop through all files
             {
-                string file = files[i]; //current file
-                for(int j = 0; j < extensions.Length; j++) //Loop through all extensions for each file
+                string file = files[i]; // current file
+                for (int j = 0; j < extensions.Length; j++) // Loop through all extensions for each file
                 {
                     string footer = file.Substring(file.Length - extensions[j].Length);
-                    //If the file ends with an extension from our extensions array
+                    // If the file ends with an extension from our extensions array
                     if (footer.ToString() == extensions[j].ToString())
                     {
-                        output.Add(files[i]); //Add it to the output
+                        output.Add(files[i]); // Add it to the output
                     }
                 }
-
             }
 
-            return output.ToArray(); //Return a list of all attachments
+            return output.ToArray(); // Return a list of all attachments
         }
-
         #endregion
 
         #region Send Email
-
-        public static void Send(string sendTo) //Send email function. This is where most of the work starts
+        public static void Send(string sendTo)
         {
-            string[] getAttachments = Attachments(); //Gets all valid attachment files in Pictures folder
+            string[] getAttachments = Attachments(); // Gets all valid attachment files in Pictures folder
 
             try
             {
-                System.Net.Mail.Attachment attachment; //Create attachment variable
-                MailMessage mail = new MailMessage(); //Create mail variable
-                SmtpClient SmtpServer = new SmtpClient(smtp_host); //Create SmtpClient
+                System.Net.Mail.Attachment attachment; // Create attachment variable
+                MailMessage mail = new MailMessage(); // Create mail variable
+                SmtpClient SmtpServer = new SmtpClient(smtp_host); // Create SmtpClient
 
-                //Load smtp/email settings from Configuration file
+                // Load smtp/email settings from Configuration file
                 SmtpServer.Port = smtp_port;
                 mail.From = new MailAddress(mail_from);
                 mail.Subject = mail_subject;
                 mail.Body = mail_body;
 
-                mail.To.Add(sendTo.ToString()); //Get sendTo email from user input
+                mail.To.Add(sendTo.ToString()); // Get sendTo email from user input
 
-                for(int i = 0; i < getAttachments.Length; i++) //Loop through each attachment
+                for (int i = 0; i < getAttachments.Length; i++) // Loop through each attachment
                 {
-                    attachment = new System.Net.Mail.Attachment(getAttachments[i]); //Set the attachment variable
-                    mail.Attachments.Add(attachment); //Attach the new attachment to the email
+                    attachment = new System.Net.Mail.Attachment(getAttachments[i]); // Set the attachment variable
+                    mail.Attachments.Add(attachment); // Attach the new attachment to the email
                 }
 
-                SmtpServer.Credentials = new System.Net.NetworkCredential(smtp_user, smtp_pass); //Login to SMTP
-                SmtpServer.EnableSsl = true; //Use SSL Encryption
+                SmtpServer.Credentials = new System.Net.NetworkCredential(smtp_user, smtp_pass); // Login to SMTP
+                SmtpServer.EnableSsl = true; // Use SSL Encryption
 
-                SmtpServer.Send(mail); //Send our mail variable
-                MessageBox.Show(mail_delivery + sendTo); //Popup informs user where the email was sent
+                SmtpServer.Send(mail); // Send our mail variable
+                MessageBox.Show(mail_delivery + sendTo); // Popup informs user where the email was sent
             }
-            catch (Exception ex) //If the Try fails the catch will output this error message.
+            catch (Exception ex) // If the Try fails the catch will output this error message.
             {
                 MessageBox.Show("A critical error has occured please contact IT \r\n" + ex.ToString());
             }
         }
-
         #endregion
 
         #endregion
     }
 }
-
-
-#region [ARCHIVE]
-/* This code is commented out, but it might be 
- * useful if we ever need to work on the software 
- * again in the future, so I've kept it archived. */
-
-#region [ARCHIVE] Encryption, Decryption
-/*
-//Methods used to encrypt and decrypt a string
-//Source https://www.delftstack.com/howto/csharp/encrypt-and-decrypt-a-string-in-csharp/
-
-public static string Encrypt(string input)
-{
-    try
-    {
-        string textToEncrypt = input;
-        string ToReturn = "";
-        byte[] secretkeyByte = { };
-        secretkeyByte = System.Text.Encoding.UTF8.GetBytes(key_secret);
-        byte[] publickeybyte = { };
-        publickeybyte = System.Text.Encoding.UTF8.GetBytes(key_public);
-        MemoryStream ms = null;
-        CryptoStream cs = null;
-        byte[] inputbyteArray = System.Text.Encoding.UTF8.GetBytes(textToEncrypt);
-        using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
-        {
-            ms = new MemoryStream();
-            cs = new CryptoStream(ms, des.CreateEncryptor(publickeybyte, secretkeyByte), CryptoStreamMode.Write);
-            cs.Write(inputbyteArray, 0, inputbyteArray.Length);
-            cs.FlushFinalBlock();
-            ToReturn = Convert.ToBase64String(ms.ToArray());
-        }
-        return ToReturn;
-    }
-    catch (Exception ex)
-    {
-        throw new Exception(ex.Message, ex.InnerException);
-    }
-}
-
-static string Decrypt(string input)
-{
-    try
-    {
-        string textToDecrypt = input;
-        string ToReturn = "";
-        byte[] privatekeyByte = { };
-        privatekeyByte = System.Text.Encoding.UTF8.GetBytes(key_secret);
-        byte[] publickeybyte = { };
-        publickeybyte = System.Text.Encoding.UTF8.GetBytes(key_public);
-        MemoryStream ms = null;
-        CryptoStream cs = null;
-        byte[] inputbyteArray = new byte[textToDecrypt.Replace(" ", "+").Length];
-        inputbyteArray = Convert.FromBase64String(textToDecrypt.Replace(" ", "+"));
-        using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
-        {
-            ms = new MemoryStream();
-            cs = new CryptoStream(ms, des.CreateDecryptor(publickeybyte, privatekeyByte), CryptoStreamMode.Write);
-            cs.Write(inputbyteArray, 0, inputbyteArray.Length);
-            cs.FlushFinalBlock();
-            Encoding encoding = Encoding.UTF8;
-            ToReturn = encoding.GetString(ms.ToArray());
-        }
-        return ToReturn;
-    }
-    catch (Exception ae)
-    {
-        throw new Exception(ae.Message, ae.InnerException);
-    }
-}
-*/
-#endregion
-
-#region [ARCHIVE] Encrypt/Decrypt Files
-/*
-public static void WriteEncryptedFile(string[] data, string fileName, string fileExtension)
-{
-
-    string temp = "";
-    string file = fileName + "." + fileExtension;
-
-    // Write the string array to a new file named "WriteLines.txt".
-    using (StreamWriter outputFile = new StreamWriter(Path.Combine(appPath, file)))
-    {
-        foreach (string line in data)
-        {
-            temp = Encrypt(line);
-        }
-        outputFile.WriteLine(temp);
-    }
-}
-
-public static string[] ReadEncryptedFile(string fileName, string fileExtension)
-{
-    string temp = "";
-    string file = fileName + "." + fileExtension;
-
-    string[] lines = System.IO.File.ReadAllLines(Path.Combine(appPath, file));
-    string[] output = new string[lines.Length];
-
-    for (int i = 0; i < lines.Length; i++)
-    {
-        temp = Decrypt(lines[i]);
-        output[i] = temp;
-    }
-
-    return output;
-}
-
-*/
-#endregion
-
-#region [ARCHIVE] Keygen 
-/*
-//Used origonally do generate public and secret keys
-public static string GenerateKey(int length)
-{
-    //Random
-    Random rand = new Random();
-    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    return new string(Enumerable.Repeat(chars, length).Select(s => s[rand.Next(s.Length)]).ToArray());
-}
-*/
-#endregion
-
-#endregion
